879ecfc033425a689cb68af62699295b06989eda
victor carrillo <gatitto079@gmail.com>
2024-08-27 00:06:57 -0600

    fix: :bug: prube test
    .

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,24 +1,3 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    'header-max-length': [2, 'always', 100],
-    'type-enum': [2, 'always', [
-      'build',
-      'chore',
-      'ci',
-      'docs',
-      'feat',
-      'fix',
-      'perf',
-      'refactor',
-      'style',
-      'test',
-      // Permitir emojis en el tipo de commit
-      'emojis',
-    ]],
-    'subject-empty': [2, 'never'],
-    'subject-case': [2, 'always', 'lower-case'],
-    'type-case': [2, 'always', 'lower-case'],
-    'type-empty': [2, 'never']
-  }
 };
