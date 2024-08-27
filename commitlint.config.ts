1263b80597861384e6d30bf1730c012cb31425a8
victor carrillo <gatitto079@gmail.com>
2024-08-26 23:58:53 -0600

    fix: :zap: added release
    test

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,3 +1,24 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 100],
+    'type-enum': [2, 'always', [
+      'build',
+      'chore',
+      'ci',
+      'docs',
+      'feat',
+      'fix',
+      'perf',
+      'refactor',
+      'style',
+      'test',
+      // Permitir emojis en el tipo de commit
+      'emojis',
+    ]],
+    'subject-empty': [2, 'never'],
+    'subject-case': [2, 'always', 'lower-case'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never']
+  }
 };
